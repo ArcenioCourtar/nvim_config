7e18669cfbcc14569f53948e64f66777b9dad6e5
Arcenio Courtar <71286201+ArcenioCourtar@users.noreply.github.com>
2025-06-21 19:41:35 +0200

    added completion engine, seems buggy on some systems

---
+++ lua/plugins/completion.lua
@@ -0,0 +1,35 @@
+return {
+	{
+		"saghen/blink.cmp",
+		dependencies = {
+			{ "rafamadriz/friendly-snippets", },
+			{
+				"L3MON4D3/luasnip",
+				build = "make install_jsregexp",
+				config = function()
+					require("luasnip.loaders.from_vscode").lazy_load()
+					require("luasnip.loaders.from_lua").lazy_load({ paths = "./lua/snippets"})
+				end
+			},
+		},
+		version = '1.*',
+
+		---@module "blink.cmp"
+		---@type blink.cmp.config
+		opts = {
+			snippets = { preset = 'luasnip' },
+			keymap = {
+				preset = 'default',
+			},
+			appearance = {
+				nerd_font_variant = 'mono',
+			},
+			completion = { documentation = { auto_show = false } },
+			sources = {
+				default = { 'lsp', 'path', 'snippets', 'buffer' },
+			},
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+		},
+		opts_extend = { 'sources.default' }
+	},
+}
